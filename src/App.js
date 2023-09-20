3024aa60795c146680f5da465de98dfff31c8ced
Jamwalaryan <aryanjamwal29@gmail.com>
2023-09-20 11:33:18 +0530

    assignment1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import {Routes,Route} from 'react-router-dom';
+import Dashboard from './Pages/Dashboard';
+import About from './Pages/About';
+import FAQ from './Pages/FAQ';
+import Perk from './Pages/Perk';
+import Support from './Pages/Support';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Sidebar>
+      <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ/>} />
+          <Route path="/perk" element={<Perk/>} />
+          <Route path="/support" element={<Support />} />
+        </Routes>
+        </Sidebar>
+        <Footer/>
     </div>
   );
 }
